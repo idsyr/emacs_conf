d8006d386632cab1ec6082ec558a9529bf7282f7
idsyr <idsyr@mail.ru>
2024-08-01 23:24:11 +0500

    Current Emacs config

---
+++ init.el
@@ -0,0 +1,18 @@
+(org-babel-load-file
+  (expand-file-name
+    "config.org"
+    user-emacs-directory))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("8013c9938a724890ba197f3585875b9736e121aaf93f616e47c3d38b8a4d24fe" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
